db2915df0b10916bb82ff846031fa39659ccdb00
gun5046 <gun5046@naver.com>
2019-03-31 19:59:42 +0900

    week3

---
+++ week3-2/homework4.2.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+
+var link="homework.html"
+document.querySelector("sp.gallery").addEventListener("click",function(){
+	<input type="button" value="네이버로 이동" onclick="location.href='http://www.naver.com';" />
+
+})
